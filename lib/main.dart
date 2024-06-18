36e19141a60772ad7486c50997bbe7ad56ca44bd
Abinadi Sanchez <abinadi111@gmail.com>
2024-06-18 13:01:43 -0600

    Crear la instancia del repositorio - Riverpod

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 
 import 'package:cinemapedia/config/router/app_router.dart';
 import 'package:cinemapedia/config/theme/app_theme.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart'; 
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart'; 
 //Se instaló el paquete flutter_dotenv junto con el ASSETS en archivo YAML | clase 12.- ENVOIRMENT VARIABLES Y GIT
 //Ya que GIT no reconece archiv .ENV y se hizo este proceso
 
@@ -11,9 +12,10 @@ Future <void> main() async {
 
   await dotenv.load(fileName: '.env'); //Se ejecutan las variables de entorno y ya las podemos utilizar
                                       //si cambiamos las variables de entorno EN TIEMPO DE EJECUCIÓN NO SE VERÁ CAMBIOS
-  runApp(const MainApp());
-
-
+  runApp(
+    const ProviderScope( child: MainApp() ) //!Al instalar RIVERPOD se debe configurar esto
+                                            //! PROVIDERSCOPE contendrá todos los providers de RIVERPOD
+    );
 }
 
 class MainApp extends StatelessWidget {
---
+++ lib/presentation/providers/movies/movies_repository_provider.dart
@@ -0,0 +1,20 @@
+
+import 'package:cinemapedia/infrastructure/datasources/moviedb_datasource.dart';
+import 'package:cinemapedia/infrastructure/repositories/movie_repository_impl.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+// Este archivo creara una instancia de MOVIE REPOSITORY IMPLEMENTATION
+
+// Este PROVIDER ya no será cambiado nunca más | lo cual puede ser un PROVIDER sólo de lectura 
+// Se instala el paquete: flutter_riverpod | CTRL + SHIFT + P => PUSPEC ASSIST => flutter_riverpod
+
+
+// Este REPOSITORIO es inmutable
+//* Su objeto es proporcionar a todos los demás PROVIDERS la info. necesaria para que puedan...
+//*consultar la info de MOVIEREPOSITORYIMPL (en este caso sólo es el GETNOWPLAYING lo que está en esa clase)
+
+final movieRepositoryProvider = Provider((ref) { // PROVIDER = Es de sólo lectura
+
+return MovieRepositoryImpl( MoviedbDatasource() ); // entre paréntesis pide el ORIGEN DE DATOS para que funcione el MOVIEREPOSITORYPROVIDER
+                                        // es el corazón de todo lo que hemos estado haciendo
+});
