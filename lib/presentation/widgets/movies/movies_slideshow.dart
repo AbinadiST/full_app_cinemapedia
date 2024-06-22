2a809b50ebce03b427b712cde01351a279901d79
Abinadi Sanchez <abinadi111@gmail.com>
2024-06-22 12:45:13 -0600

    Terminar Carrusel de películas | SwiperPagination

--- lib/presentation/widgets/movies/movies_slideshow.dart
+++ lib/presentation/widgets/movies/movies_slideshow.dart
@@ -1,9 +1,13 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:card_swiper/card_swiper.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 
 // CTRL + SHIFT + P 
 // PUBSPEC ASSISTE => ingresar: card_swiper | Para mostrar imágenes en carrusel
+
+// CTRL + SHIFT + P 
+// PUBSPEC ASSISTE => ingresar: animate_do | para animaciones
 
 class MoviesSlideshow extends StatelessWidget {
 
@@ -15,6 +19,9 @@ class MoviesSlideshow extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    final colors = Theme.of(context).colorScheme;
+
     return SizedBox(
       height: 210,
       width: double.infinity, //toma todo el espacio restante
@@ -22,6 +29,14 @@ class MoviesSlideshow extends StatelessWidget {
         viewportFraction: 0.8, // con esto podemos ver el Slide anterior y un pedacito del que sigue
         scale: 0.9, //tiene un efecto donde los slide de alado se ve un poco chico al slide principal
         autoplay: true, // se mueve en automático los slides
+        pagination: SwiperPagination(  //SwiperPagination.fraction = muestra el # de imagen que se muestra ej, 5/20
+          //* Personalizaremos el SWIPERPAGINATION
+          margin: const EdgeInsets.only( top: 0 ), // para poner los cirulitos hasta abajo del área del SWIPER
+          builder: DotSwiperPaginationBuilder( 
+            activeColor: colors.primary,
+            color: colors.secondary
+          )
+        ), 
         itemCount: movies.length,
         itemBuilder: (context, index) => _Slide(movie: movies[index]),
 
@@ -45,6 +60,44 @@ class _Slide extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+
+    final decoration = BoxDecoration(
+      borderRadius: BorderRadius.circular(20),
+      boxShadow: const [
+        BoxShadow(
+          color: Colors.black45,
+          blurRadius: 10, //esto es un difuminado|desvanecido en el marco
+          offset: Offset(0, 10) //sombra donde los valores son X y Y 
+
+        )
+      ]
+    );
+
+
+    return Padding(
+      padding: const EdgeInsets.only( bottom: 30), //queda un poco estrecho en la parte inferior
+      child: DecoratedBox(
+        decoration: decoration,
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(20),  // para colocar borders redondos
+          child: Image.network(
+            movie.backdropPath,
+            fit: BoxFit.cover, //para que la imagen se aprecie mejor dentro del SLIDE que hicimos
+            loadingBuilder: (context, child, loadingProgress) {
+              if ( loadingProgress != null ) {
+
+                //* se indica que el progress está cargando
+
+                return const DecoratedBox(
+                  decoration: BoxDecoration( color: Colors.black12 )
+                );
+              }
+
+              return FadeIn(child: child); // si ya terminó la carga, la imagen entra con suavidad | instalar paquete ANIMATE_DO
+            },
+            )
+          ),
+        ),
+      );
   }
 }
