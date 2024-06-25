5ced18c6e4e6af9365757e6dbbac552202ea8487
Abinadi Sanchez <abinadi111@gmail.com>
2024-06-25 12:41:24 -0600

    Mostrar las películas en el ListView

--- lib/presentation/widgets/movies/movie_horizontal_listview.dart
+++ lib/presentation/widgets/movies/movie_horizontal_listview.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
 
@@ -26,12 +27,101 @@ class MovieHorizontalListview extends StatelessWidget {
            if ( title != null || subTitle != null )
            _Title( title: title, subTtile: subTitle ),
 
+
+          //* Scroll horizonatal ***
+
+           Expanded(
+            child: ListView.builder(
+              itemCount: movies.length,
+              scrollDirection: Axis.horizontal,
+              physics: const BouncingScrollPhysics(), // para que se vea igual en Android i iOS
+              itemBuilder: (context, index) {
+                
+                return _Slide(movie: movies[index]);
+              },
+              )
+            )
+
         ],
       ),
     );
   }
 }
 
+
+class _Slide extends StatelessWidget {
+
+  final Movie movie;
+
+  const _Slide({ required this.movie });
+
+  @override
+  Widget build(BuildContext context) {
+
+    final textStyles = Theme.of(context).textTheme;
+
+    return Container( // colocamos un CONTAINER porque tiene el MARGIN ya que el efecto es diferente que PADDING
+      margin: EdgeInsets.symmetric( horizontal: 8 ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start, //para que todos sus hijos estén alineados al inicio
+        children: [
+          
+          //* imagen
+          SizedBox(
+            width: 150,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.network(
+                movie.posterPath,
+                fit: BoxFit.cover, // toas las imágenes toman el mimso tamaño
+                width: 150,
+                loadingBuilder: (context, child, loadingProgress) {
+                  
+                  //* Cuando se está cargando la imagen, hacemos lo siguiente
+                  if ( loadingProgress != null ) {
+                    return const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Center(child: CircularProgressIndicator( strokeWidth: 2)
+                      ),
+                    );
+                  }
+
+                  return FadeIn(child: child); // muestra las imagenes con efecto | puede ser también con FADEINRIGHT
+
+                },
+              ),
+            ),
+          ),
+
+          const SizedBox( height: 5),
+
+          //* Title ***
+          SizedBox(
+            width: 150,
+            child: Text(
+              movie.title,
+              maxLines: 2,
+              style: textStyles.titleSmall,
+            ),
+          ),
+
+          //*Rating ***
+          Row(
+            children: [
+              Icon( Icons.star_half_outlined, color: Colors.yellow.shade800 ), //SHADE = Se altera en diferentes capas el amarillo (en este caso) 
+              const SizedBox( width: 3 ),
+              Text('${ movie.voteAverage }', style: textStyles.bodyMedium?.copyWith( color: Colors.yellow.shade800 )
+              ),
+              const SizedBox( width: 10 ),
+              Text('${ movie.popularity }', style: textStyles.bodySmall ),
+            ],
+          )
+
+        ],
+      ),
+    );
+  }
+}
 
 class _Title extends StatelessWidget {
   
